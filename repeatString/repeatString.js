00ef3c7aa2e86d9a4f99190df56dfdcbb8635714
vang37 <vang37@gmail.com>
2019-07-06 13:22:10 +0800

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    if (num < 0){return 'ERROR'};
+    let text = '';
+    for (i = 0; i < num; i++) {
+        text = text + str;
+    }
+    return text;
 }
-
 module.exports = repeatString
