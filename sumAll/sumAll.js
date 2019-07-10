0a75df61a3ff2cf3808473068410e20811823743
vang37 <vang37@gmail.com>
2019-07-10 19:37:42 +0800

    Update sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+ 
+    if (isNaN(a) || isNaN(b)) {
+        return 'ERROR';
+    }
+    else if (typeof a === "string" || typeof b === "string") {
+        return 'ERROR';
+    }
+    else if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+    else if (a > b) {
+        return (((a - b)*(a + b +1)) / 2) + 1;
+    }
+    else if (b > a) {
+        return (((b - a)*(a + b +1)) / 2) + 1;
+    }
+    else if (a == b) {
+        return "0";
+    }
+    
+    
 }
 
 module.exports = sumAll
