395116f1730b3a9989b995294e8ea6466357c091
vang37 <vang37@gmail.com>
2019-07-11 13:15:10 +0800

    Update tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp1) {
+  let f = parseFloat(temp1); 
+  var x = (f - 32) * (5 / 9);
+  return parseFloat(x.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(temp2) {
+  let c = parseFloat(temp2);
+  var y = (c * (9 / 5)) + 32;
+  return parseFloat(y.toFixed(1));
 }
 
 module.exports = {
