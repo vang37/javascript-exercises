2bec7f0cee8953796b022a184405bd36e3d606b8
vang37 <vang37@gmail.com>
2019-07-16 17:01:31 +0800

    Update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let a = 1;
+    let b = 1;
+    array = []
+    if (num < 0){return "OOPS"}
+    else
+    for (i = 2; i < num; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+        array.push(c);
+    }
+    return array[array.length - 1]
 }
 
 module.exports = fibonacci
