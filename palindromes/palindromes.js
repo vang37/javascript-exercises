c453c4e5fa89f5f14cd7d08d7c5e9aae5afe5803
vang37 <vang37@gmail.com>
2020-07-21 13:55:15 +0800

    There's a better way to do this, but this one's easy(-ier) to read.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function() {
-
+const palindromes = function (str) {
+    let string = "", reverseString = "", array = [];
+    for (let s of str) {
+        if (isAlpha(s)) {
+            array.push(s)
+        }
+    }
+    for (let a of array) {
+        string += a.toLowerCase();
+        reverseString = a + reverseString.toLowerCase();
+    }
+    if (string === reverseString) return true
+    else return false
 }
 
+function isAlpha(aChar) {
+    return aChar.match(/[a-z]/i);
+}
+
+
 module.exports = palindromes
+
+
