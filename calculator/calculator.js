2d72ea59f45fbef97f107cbce711039b37f0640d
vang37 <vang37@gmail.com>
2020-07-21 14:28:36 +0800

    Calculator functions? Check. (Probably a better way to do these, but this was easy.)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,50 @@
-function add () {
-	
+function add(a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum(array) {
+	let start = 0;
+	if (array.length == 0) return 0;
+	for (let arr of array) {
+		start += arr;
+	}
+	return start;
+
 }
 
-function multiply () {
-	
+function multiply(array) {
+	let start = 1;
+	if (array.length == 0) return 0;
+	if (array.length == 1) return array[0];
+	for (let arr of array) {
+		start *= arr;
+	}
+	return start;
 }
 
-function power() {
-	
+function power(base, exp) {
+	let val = base;
+	if (base != 0 && exp == 0) return 1;
+	while (exp > 1) {
+		val *= base;
+		exp--;
+	}
+	return val;
 }
 
-function factorial() {
-	
+
+function factorial(n) {
+	if (n == 0 || n < 0) return 1;
+	let fact = n;
+	while (n > 1) {
+		n--;
+		fact *= n;
+	}
+	return fact;
 }
 
 module.exports = {
@@ -27,6 +52,6 @@ module.exports = {
 	subtract,
 	sum,
 	multiply,
-    power,
+	power,
 	factorial
 }
