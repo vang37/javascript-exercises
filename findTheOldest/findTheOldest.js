91a847326457805adb3b111b1834d874b14302d0
vang37 <vang37@gmail.com>
2020-07-21 14:04:18 +0800

    An exercise to test one's comfort with reduce.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-let findTheOldest = function() {
 
+
+
+let findTheOldest = function(array) {
+    let max;
+    array.reduce((a, b) => {
+        if (getAge(b) >= a) {
+            max = b;
+            return Math.max(a, getAge(b));
+        }
+        else return Math.max(a, getAge(b));
+    }, getAge(array[0]));
+    return max
+
+
+    function getAge(object) {
+        if (!("yearOfDeath" in object)) {
+            object["yearOfDeath"] = new Date().getFullYear();
+            return object.yearOfDeath - object.yearOfBirth;
+        }
+        else if (!object.yearOfBirth) return "Person does not exist yet. ALso: existence not guaranteed.";
+        else return object.yearOfDeath - object.yearOfBirth;
+    }
 }
 
 module.exports = findTheOldest
