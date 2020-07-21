58e991d180beb9dd4d53c5be0610f75e8fa34b4a
vang37 <vang37@gmail.com>
2020-07-21 13:32:28 +0800

    re-organized with value-added function.

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,35 +1,23 @@
 const caesar = function (str, num) {
-    let newArray = [];
-    let newArray1 = [];
-    let newArray2 = [];
-    var array = str.split(/(\s+)/);
+    let array_from_string = [];
 
-    for (i = 0; i < array.length; i++) {
-        var n = array[i];
-        for (k = 0; k < n.length; k++) {
-            var x = n.charCodeAt(k);
-            newArray.push(x);
-        }
+    for (let i = 0; i < str.length; i++) {
+        array_from_string[i] = String.fromCharCode(valueCheck(str[i].charCodeAt()));
     }
 
-    newArray1 = newArray.map(
-        function (value) {
-            if (((value > 64) && (value < 91)) && (((value + num % 26) > 64) && ((value + num % 26) < 91))) { return value + num % 26 }
-            else if (((value > 96) && (value < 123)) && (((value + num % 26) > 96) && ((value + num % 26) < 123))) { return value + num % 26 }
-            else if ((value > 64) && (value < 91) && (num > 0)) { return value - 26 + num % 26 }
-            else if ((value > 64) && (value < 91) && (num < 0)) { return value + 26 + num % 26 }
-            else if ((value > 96) && (value < 123) && (num > 0)) { return value - 26 + num % 26 }
-            else if ((value > 96) && (value < 123) && (num < 0)) { return value + 26 + num % 26 }
-            return value;
-        }
-    )
+    return array_from_string.join("");
 
-    for (j = 0; j < newArray1.length; j++) {
-        var val = newArray1[j];
-        newArray2.push(String.fromCharCode(val));
+    function valueCheck(value) {
+        if (((value > 64) && (value < 91)) && (((value + num % 26) > 64) && ((value + num % 26) < 91))) { return value + num % 26 }
+        else if (((value > 96) && (value < 123)) && (((value + num % 26) > 96) && ((value + num % 26) < 123))) { return value + num % 26 }
+        else if ((value > 64) && (value < 91) && (num > 0)) { return value - 26 + num % 26 }
+        else if ((value > 64) && (value < 91) && (num < 0)) { return value + 26 + num % 26 }
+        else if ((value > 96) && (value < 123) && (num > 0)) { return value - 26 + num % 26 }
+        else if ((value > 96) && (value < 123) && (num < 0)) { return value + 26 + num % 26 }
+        else return value;
     }
-
-    return newArray2.join("");
 }
+
+
 
 module.exports = caesar
