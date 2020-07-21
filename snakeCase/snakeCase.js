f7f51f41aaeecd3e9de4bf7bac9808bd65b06d58
vang37 <vang37@gmail.com>
2020-07-21 14:19:51 +0800

    Yeah, regex is the way to go ...

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,20 @@
-const snakeCase = function() {
-
+const snakeCase = function (str) {
+    if ((/([a-zA-Z0-9][~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-\s]+)/g).exec(str)) {
+        for (i = str.length - 1; ;) {
+            if ((/[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-\s]+/).exec(str[i])) {
+                str = str.slice(0, -1);
+                i--;
+            }
+            else break;
+        }
+        return str.split(/[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-\s]+/).join("_").toLowerCase();
+    }
+    if ((/([a-z][A-Z0-9])+/g).exec(str)) {
+        let ind = (/([a-z][A-Z])/g).exec(str).index;
+        let left = str.slice(0, ind + 1).toLowerCase();
+        let right = str.slice(ind + 1).toLowerCase();
+        return left + "_" + right;
+    }
 }
 
 module.exports = snakeCase
